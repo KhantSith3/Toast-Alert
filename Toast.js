de6af0d97aa6ad8c357eea5d13f3fd6ae6c0a3e3
Khant Si Thu <khantsithu477@gmail.com>
2022-12-23 11:00:49 +0630

    updated

--- Toast.js
+++ Toast.js
@@ -1,20 +1,20 @@
 const ButtonTag = document.querySelector(".button");
-const ParentTag =document.querySelector(".parent");
-const containerTag =document.querySelector(".container");
+const ParentTag = document.querySelector(".parent");
+const containerTag = document.querySelector(".container");
 
-ButtonTag.addEventListener("click",() =>{
-    ParentTag.innerHTML="";
-containerTag.classList.add("darkmood");
- const DivTag =document.createElement("div");
- DivTag.classList.add("Mery");
- ParentTag.append(DivTag);
- DivTag.append (`You are turning night Mood`);
- DivTag.style.bottom =`-${DivTag.offsetHeight}px`;
- 
- setTimeout(()=>{
-    DivTag.style.bottom =`0px`;
- },100);
-setTimeout(()=>{
-   DivTag.style.bottom =`-${DivTag.offsetHeight}px`;
-},2000);
+ButtonTag.addEventListener("click", () => {
+  ParentTag.innerHTML = "";
+  containerTag.classList.add("darkmood");
+  const DivTag = document.createElement("div");
+  DivTag.classList.add("Mery");
+  ParentTag.append(DivTag);
+  DivTag.append(`You are turning night Mood`);
+  DivTag.style.bottom = `-${DivTag.offsetHeight}px`;
+
+  setTimeout(() => {
+    DivTag.style.bottom = `0px`;
+  }, 100);
+  setTimeout(() => {
+    DivTag.style.bottom = `-${DivTag.offsetHeight}px`;
+  }, 2000);
 });
